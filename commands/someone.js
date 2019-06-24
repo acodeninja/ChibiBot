a9940bdc4e88b2a0e537bdc7f40ed871ad78c1e5
JosephAnime <51287094+JosephAnime@users.noreply.github.com>
2019-06-24 19:30:09 +0200

    Create someone.js

---
+++ commands/someone.js
@@ -0,0 +1,11 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+    let user = message.guild.members.random();
+    while (user.user.bot) user = message.guild.members.random();
+    message.channel.send(`:thinking: C'est sûrement ${user}!`)
+}
+
+module.exports.help = {
+  name: "someone"
+}
